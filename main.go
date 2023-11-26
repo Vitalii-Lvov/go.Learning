20b97de04224ee6b7b269158f9950c8238537a58
Vitalii-Lvov <lvovvitaly@yandex.ru>
2023-11-26 14:13:13 +0300

    loop an wait groups

--- main.go
+++ main.go
@@ -7,20 +7,33 @@ import (
 
 func main() {
 
-	testLoop()
+	testLoop(nil)
 
 	var wg sync.WaitGroup
 	wg.Add(1)
 
-	go testLoop()
+	go testLoop(&wg)
+
+	prn("waiting began")
 
 	wg.Wait()
+
+	prn("waitnig end")
+
 }
 
-func testLoop() {
+func testLoop(wg *sync.WaitGroup) {
 
 	for i := 0; i < 3; i++ {
 		fmt.Println(i)
 	}
 
+	if wg != nil {
+		wg.Done()
+	}
+
+}
+
+func prn(msg any) {
+	fmt.Println(msg)
 }
